aee37fc596ea6c85c68f8f802cb3f95bdf71974e
Reception <elenapoker888@gmail.com>
2019-05-24 17:25:04 +0400

    1710 ready

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -19,34 +19,54 @@ public class Solution {
     }
 
     public static void main(String[] args) throws ParseException {
-       // Scanner scan = new Scanner(System.in);
-      //  String s=scan.nextLine();
-        String s="-c Миронов м 15/04/1990";
-       // System.out.println(s);
 
-        String[] data=s.split(" ");
 
-        for (int i=data.length-1; i >=0; i--) {
-           // System.out.println("String["+i+"]= "+data[i]);
-        }
 
-        if (data[0].equals("-c")){
-           // System.out.println(data[0]);
+
+        String[] data=args;
+
+         if (data[0].equals("-c")){
+
             Date date = new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH).parse(data[3]);
 
-            if (data[2].equals("м")) System.out.println(allPeople.add(Person.createMale(data[1],date)));
+            if (data[2].equals("м")) allPeople.add(Person.createMale(data[1],date));
                 else  allPeople.add(Person.createFemale(data[1],date));
 
-            System.out.println("id= "+(allPeople.size()-1));
+            System.out.println((allPeople.size()-1));
+          }
+
+        if (data[0].equals("-u")){
+
+
+            Date date = new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH).parse(data[4]);
+
+            if (data[3].equals("м")) allPeople.set(Integer.parseInt(data[1]),Person.createMale(data[2],date));
+            else  allPeople.set(Integer.parseInt(data[1]),Person.createFemale(data[1],date));
+
         }
 
-        for (Person a:allPeople
-             ) {
-            System.out.print(a.getSex()+" "+a.getName()+" ");
-            SimpleDateFormat DateFormat=new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
-            String date=DateFormat.format(a.getBirthDate());
-            System.out.println(date);
+        if (data[0].equals("-d")){allPeople.get(Integer.parseInt(data[1])).setBirthDate(null);
+            allPeople.get(Integer.parseInt(data[1])).setName(null);
+            allPeople.get(Integer.parseInt(data[1])).setSex(null);
         }
+
+        if (data[0].equals("-i")) {
+            SimpleDateFormat DateFormat=new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+            Person person=allPeople.get(Integer.parseInt(data[1]));
+            System.out.println(String.format("%s %s %s",person.getName(),(person.getSex().toString()=="MALE"?"м":"ж"),DateFormat.format(person.getBirthDate())));
+
+        }
+
+
+
+
+//        for (Person a:allPeople
+//             ) {
+//            System.out.print(a.getSex()+" "+a.getName()+" ");
+//            SimpleDateFormat DateFormat=new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+//            String date=DateFormat.format(a.getBirthDate());
+//            System.out.println(date);
+//        }
 
     }
 }
