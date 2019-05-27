b95731e0d632a61351cdc7d78bf29900db4c961e
mahamantra <elenapoker888@gmail.com>
2019-05-27 21:41:39 +0400

    1628

--- 2.JavaCore/src/com/javarush/task/task16/task1628/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1628/Solution.java
@@ -8,28 +8,35 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
-    public static volatile AtomicInteger countReadStrings = new AtomicInteger(0);
+    public static volatile AtomicInteger readStringCount = new AtomicInteger(0);
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         //read count of strings
+        System.out.println("Enter count");
         int count = Integer.parseInt(reader.readLine());
 
         //init threads
+        System.out.println("create Thread");
         ReaderThread consolReader1 = new ReaderThread();
         ReaderThread consolReader2 = new ReaderThread();
         ReaderThread consolReader3 = new ReaderThread();
 
+
+        System.out.println("Thread Start");
         consolReader1.start();
         consolReader2.start();
         consolReader3.start();
 
-        while (count > countReadStrings.get()) {
+        while (count > readStringCount.get()) {
         }
 
+        System.out.println("invoked interrupt()");
         consolReader1.interrupt();
         consolReader2.interrupt();
         consolReader3.interrupt();
+
+        System.out.println("Print");
         System.out.println("#1:" + consolReader1);
         System.out.println("#2:" + consolReader2);
         System.out.println("#3:" + consolReader3);
@@ -41,14 +48,19 @@ public class Solution {
         private List<String> result = new ArrayList<String>();
 
         public void run() {
-            while (!isInterrupted()){
-                try {
-                    result.add(reader.readLine());
-                    countReadStrings.incrementAndGet();
+            //System.out.println("run Start"+Thread.currentThread().getName());
 
-                } catch (IOException e) {
+            while (!isInterrupted()) {
+
+                try {
+                    if (reader.ready()){
+                    result.add(reader.readLine());
+                    readStringCount.incrementAndGet();}
+                    } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                //add your code here - добавьте код тут
             }
         }
 
