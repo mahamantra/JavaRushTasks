4dee3dad37431c49e14ca8609f7f28c87f2a799b
elenapoker888@gmail.com <elenapoker888@gmail.com>
2019-06-04 08:22:12 +0400

    1827 start

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -4,7 +4,39 @@ package com.javarush.task.task18.task1827;
 Прайсы
 */
 
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        Writer writer=new FileWriter("f1",true);
+        BufferedWriter bufferedWriter=new BufferedWriter(writer);
+
+       StringBuilder quantity=new StringBuilder( args[args.length-1]);
+       StringBuilder price=new StringBuilder( args[args.length-2]);
+       StringBuilder productName=new StringBuilder();
+
+        for (int i = 1; i <args.length-3 ; i++) {
+            productName.append(args[i]+" ");
+        }
+
+        System.out.println(quantity);
+        quantity.append(" ".repeat(4-quantity.length()));
+        price.append(" ".repeat(8-price.length()));
+        productName.append(" ".repeat(30-productName.length()));
+
+        System.out.println(quantity);
+        System.out.println(price);
+        System.out.println(productName);
+
+        StringBuilder data=new StringBuilder();
+        data.append(productName).append(price).append(quantity).append("\n");
+
+        System.out.println(data);
+
+        bufferedWriter.write(data.toString());
+        bufferedWriter.close();
+
+
     }
 }
