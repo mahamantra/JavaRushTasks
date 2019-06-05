55de73fb852d654b4038233a542d89c787673502
Reception <elenapoker888@gmail.com>
2019-06-05 16:04:47 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	JavaRushPlugin.properties

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -9,49 +9,32 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-       Writer writer=new FileWriter(new Scanner(System.in).nextLine(),true);
+
+        Writer writer=new FileWriter(new Scanner(System.in).nextLine(),true);
        BufferedWriter bufferedWriter=new BufferedWriter(writer);
 
-       if (args.length!=0) {
-           StringBuilder quantity = new StringBuilder(args[args.length - 1]);
-           StringBuilder price = new StringBuilder(args[args.length - 2]);
-           StringBuilder productName = new StringBuilder();
+       if (args.length!=0 && args[0].equals("-c") ){
+           StringBuilder quantity = new StringBuilder((args[args.length - 1]+"                                                 ").substring(0,4));
+           StringBuilder price = new StringBuilder((args[args.length - 2]+"                                                 ").substring(0,8));
+           StringBuilder productName = new StringBuilder((args[1]+"                                                 ").substring(0,30));
+
+
 
            StringBuilder id = new StringBuilder(returnMaxID());
 
 
-           for (int i = 1; i <= args.length - 3; i++) {
-               productName.append(args[i] + " ");
-           }
-            productName.deleteCharAt(productName.length()-1);
-
-           for (int i = 0; i <(4 - quantity.length()) ; ) {
-               quantity.append(" ");
-           }
-           for (int i = 0; i <(8 - price.length()) ; ) {
-               price.append(" ");
-           }
-           for (int i = 0; i <(30 - productName.length()) ; ) {
-               productName.append(" ");
-           }
-
-           //quantity.append(" ".repeat(4 - quantity.length()));
-           //price.append(" ".repeat(8 - price.length()));
-           //productName.append(" ".repeat(30 - productName.length()));
-           //id.append(" ".repeat(8-productName.length()));
-
-//        System.out.println(quantity.length());
-//        System.out.println(price.length());
-//        System.out.println(productName.length());
-
-           StringBuilder data = new StringBuilder();
-           data.append(id).append(productName).append(price).append(quantity).append("\n");
+           String data = String.format("%s%s%s%s",id,productName,price,quantity);
+           System.out.println(data+"!!!!");
 
 
-           bufferedWriter.write(data.toString());
+           bufferedWriter.write(data+System.lineSeparator());
+           bufferedWriter.close();
+
        }
 
-        bufferedWriter.close();
+writer.close();
+
+
 
     }
 
